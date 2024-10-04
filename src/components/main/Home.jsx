f81233f2b0e7ac8b0181be5f88a92080f56c1da0
Casariana <80796740+Casariana@users.noreply.github.com>
2024-10-04 07:40:12 -0500

    Separado el back

--- src/components/main/Home.jsx
+++ src/components/main/Home.jsx
@@ -3,12 +3,16 @@ import Header from "../Header";
 import Filtros from "./Filtros";
 import PlantillaPiscina from "./PlantillaPiscina";
 import Footer from "../Footer";
-import PruebaBack from "../PruebaBack"
+
 
 export default function Home() {
   return (
     <>
-      <PruebaBack />
+      <Header />
+      <Filtros />
+      <PlantillaPiscina />
+      <Footer />
+      
       
     </>
   );
